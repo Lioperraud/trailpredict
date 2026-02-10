dcb7a876c8270df58d7f4d70c8c296ddb431e1a0
Lioperraud <lioperraud@gmail.com>
2026-02-10 18:17:43 +0100

    Initialisation du projet

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="font-inter h-screen flex flex-col items-center justify-center ">
+      <h1 className="text-6xl bg-brand">TrailPredict</h1>
+      <h2 className="text-xl italic bg-secondary">
+        Le chrono avant les courbatures.
+      </h2>
+    </div>
+  )
+}
+
+export default App
