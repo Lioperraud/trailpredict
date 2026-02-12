6f9b7d0e405da03ec3db87fb1a4649cea7966e8c
Lioperraud <lioperraud@gmail.com>
2026-02-12 14:54:32 +0100

    Correction className / Button

--- src/components/ButtonSecondary.jsx
+++ src/components/ButtonSecondary.jsx
@@ -3,7 +3,7 @@ function ButtonSecondary({ libelle, onclick = () => {}, disabled = false }) {
     <button
       onClick={onclick}
       disabled={disabled}
-      className="px-8 py-2 bg-linear-311 bg-white text-gray-800 border-1 border-gray-800 font-medium rounded-lg cursor-pointer hover:bg-gray-50 disabled:opacity-30"
+      className="px-8 py-2 bg-linear-311 bg-white text-gray-800 border border-gray-800 font-medium rounded-lg cursor-pointer hover:bg-gray-50 disabled:opacity-30"
     >
       {libelle}
     </button>
