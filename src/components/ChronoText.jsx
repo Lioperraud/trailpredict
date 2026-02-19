e8b72e5b81c648470af535c8b87ee26152daa7a9
Lioperraud <lioperraud@gmail.com>
2026-02-19 17:38:35 +0100

    Fix taille icone

--- src/components/ChronoText.jsx
+++ src/components/ChronoText.jsx
@@ -1,7 +1,7 @@
 function ChronoText({ Ico, titre, chrono, precisions, highlighting = false }) {
   const divFirstClass = `h-full flex flex-col justify-between ${highlighting ? 'text-white' : 'text-gray-600'}`
   const h3Class = `flex flex-row gap-2 items-center font-medium ${highlighting ? 'text-2xl' : 'text-xl'}`
-  const IcoClass = `h-auto ${highlighting ? '"w-6 fill-white' : 'w-5  fill-gray-600'}`
+  const IcoClass = `h-auto  ${highlighting ? 'w-6 fill-white' : 'w-5  fill-gray-600'}`
   const h4Class = `${highlighting ? 'text-8xl' : 'text-6xl'}`
   const h5Class = `uppercase ${highlighting ? 'text-teal-300 text-sm' : 'text-xs'}`
   return (
